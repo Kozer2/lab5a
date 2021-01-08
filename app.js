1b76de3c64294e422a908603c035325d01b6808e
Kozer2 <benhemann@yahoo.com>
2021-01-08 15:22:30 -0600

    Completed question 2

--- app.js
+++ app.js
@@ -32,11 +32,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+    var mulArray = [];
+    var mulTot = a * b;
+    var stringAnsw = ('The product of ' + a + ' and ' + b + ' is ' + mulTot + '.');
+    mulArray.push(mulTot);
+    mulArray.push(stringAnsw);
+    console.log(mulArray);
 
+    return(mulArray);
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
